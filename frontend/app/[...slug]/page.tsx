dcb16b61311680f65a210d7750d98d8704b0d33d
Sylester-Oputa <soputa42@gmail.com>
2026-02-04 13:23:55 +0100

    refactor: simplify page export by removing dynamic import and component wrapper

--- frontend/app/[...slug]/page.tsx
+++ frontend/app/[...slug]/page.tsx
@@ -1,11 +1,3 @@
 "use client";
 
-import dynamic from "next/dynamic";
-
-const ClientApp = dynamic(() => import("../ClientApp"), {
-  ssr: false,
-});
-
-export default function CatchAllPage() {
-  return <ClientApp />;
-}
+export { default } from "../page";
